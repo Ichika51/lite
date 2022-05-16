3d4b9717a0556ed3edcb490bff10e980dd79d67e
Ichika51 <yaroslaw.saly@yandex.ru>
2022-05-16 14:20:01 +0300

    Update stolar1.cpp

--- stolar1.cpp
+++ stolar1.cpp
@@ -4,6 +4,21 @@
 
 using namespace std;
 
+int *bubble(int* string_2, int row){
+    int temp=0;
+    for (int i = 0; i < row - 1; i++) {
+        for (int j = 0; j < row - i - 1; j++) {
+            if (string_2[j] > string_2[j + 1]) {
+                // меняем элементы местами
+                temp = string_2[j];
+                string_2[j] = string_2[j + 1];
+                string_2[j + 1] = temp;
+            }
+        }
+    }
+    return string_2;
+}
+
 void printArray(int* array[][4], const int row, const int column) {
     cout << "\n\nPoluchennyj massiv\n";
     for (int i = 0; i < row; i++){
@@ -14,6 +29,7 @@ void printArray(int* array[][4], const int row, const int column) {
 }
 
 int main() {
+    setlocale(LC_ALL, "RUSSIAN");
     const int row = 4, column = 5;
     int array[row][column];
     ifstream f("array.txt");
@@ -23,7 +39,7 @@ int main() {
         }
     }
     //prinr array
-    cout << "\n\nPoluchennyj massiv\n";
+    cout << "\n\nПолученный массив из файла:\n";
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < column; j++)
             cout << setw(4) << array[i][j];
@@ -49,7 +65,7 @@ int main() {
     for (int i = 0; i < column; i++) {
         array[0][i] = string[i];
     }
-    cout << "\n\nPoluchennyj massiv posle sortirovki 1 row\n";
+    cout << "\n\nПолученный массив после сортировки 1 строки:\n";
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < column; j++)
             cout << setw(4) << array[i][j];
@@ -61,21 +77,12 @@ int main() {
     for (int i = 0;i < row; i++) {
         string_row[i] = { array[i][0] };
     }
-     // Сортировка массива пузырьком
-    for (int i = 0; i < row - 1; i++) {
-        for (int j = 0; j < row - i - 1; j++) {
-            if (string_row[j] > string_row[j + 1]) {
-                // меняем элементы местами
-                temp = string_row[j];
-                string_row[j] = string_row[j + 1];
-                string_row[j + 1] = temp;
-            }
-        }
-    }
+
+    string_row = bubble(string_row,row);
     for (int i = 0; i < row; i++) {
         array[i][0] = string_row[i];
     }
-    cout << "\n\nPoluchennyj massiv posle sortirovki 1 column\n";
+    cout << "\n\nПолученный массив после сортировки 1 колонки:\n";
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < column; j++)
             cout << setw(4) << array[i][j];
@@ -90,53 +97,32 @@ int main() {
         string_1[i] = { array[i][i] };
     }
     // Сортировка массива пузырьком
-    for (int i = 0; i < row - 1; i++) {
-        for (int j = 0; j < row - i - 1; j++) {
-            if (string_1[j] > string_1[j + 1]) {
-                // меняем элементы местами
-                temp = string_1[j];
-                string_1[j] = string_1[j + 1];
-                string_1[j + 1] = temp;
-            }
-        }
-    }
+    string_1 = bubble(string_1,row);
     for (int i = 0; i < p; i++) {
         array[i][i] = string_1[i];
     }
-    cout << "\n\nPoluchennyj massiv posle sortirovki 1 diagonal\n";
+    cout << "\n\nПолученный массив после сортировки диагонали(с левого верхнего в правый нижний угол)\n";
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < p; j++)
             cout << setw(4) << array[i][j];
         cout << endl;
     }
-    ///////////////////////////////////////////sort diagonal bottom right to top left///////////////////////////////////////////
+    ///////////////////////////////////////////sort diagonal bottom left to top right///////////////////////////////////////////
     int* string_2 = new int[p];
     for (int i = 0;i < p; i++) {
-        string_2[i] = { array[row-i][row-i] };
+        string_2[i] = { array[row-1-i][i] };
     }
-    // Сортировка массива пузырьком
-    for (int i = 0; i < row - 1; i++) {
-        for (int j = 0; j < row - i - 1; j++) {
-            if (string_2[j] > string_2[j + 1]) {
-                // меняем элементы местами
-                temp = string_2[j];
-                string_2[j] = string_2[j + 1];
-                string_2[j + 1] = temp;
-            }
-        }
-    }
-    for (int i = 0; i < p;i++) {
-        cout << "sort: " << string_2[i] << endl;
-    }
+        // Сортировка массива пузырьком
+    string_2 = bubble(string_2,row);
+
     for (int i = 0; i < p; i++) {
-        array[row-i][row-i] = string_2[i];
+        array[row-1-i][i] = string_2[i];
     }
-    cout << "\n\nPoluchennyj massiv posle sortirovki 2 diagonal\n";
+    cout << "\n\nПолученный массив после сортировки диагонали(с левого нижнего в правый верхний угол)\n";
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < p; j++)
             cout << setw(4) << array[i][j];
         cout << endl;
     }
-    
     return 0;
 }
